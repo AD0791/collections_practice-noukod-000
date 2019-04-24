f3ceeffa989b288cb233a04db58dd3dd295a1bd6
Alexandro Disla <alexandrodisla@hotmail.com>
2019-04-24 16:25:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
+def sort_array_asc array
+  array.sort
+=begin
+array.sort do |a, b|
+a <=> b
+end
+=end
+end
 
+def sort_array_desc array
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count array
+  array.sort{|a,b| a.length <=> b.length}
+end
+
+def swap_elements array
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array array
+  array.reverse
+end
+
+def kesha_maker array
+  array.collect{|a| a[2] = "$"}
+end
